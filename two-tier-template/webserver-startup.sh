c7a7ad0a294d45bd6a0653a6c46d5cf439958d3a
Narayan Iyengar <niyengar@paloaltonetworks.com>
2018-03-01 14:41:22 -0800

    commit local changes

--- two-tier-template/webserver-startup.sh
+++ two-tier-template/webserver-startup.sh
@@ -5,6 +5,8 @@ zone=$(basename $temp)
 FW_IP=$(gcloud compute instances describe vm-series --zone=$zone --format="value(networkInterfaces[2].networkIP)")
 FW_PublicIP=$(gcloud compute instances describe vm-series --zone=$zone --format="value(networkInterfaces[1].accessConfigs[0].natIP)")
 DB_IP=$(gcloud compute instances describe db-vm --zone=$zone --format="value(networkInterfaces[0].networkIP)")
+
+echo "Is FW up?"
 while true
   do
    resp=$(curl -s -S -g --insecure "https://$FW_IP/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
@@ -14,7 +16,7 @@ while true
    fi
   sleep 10s
 done
-
+echo "Download content"
 while true
   do
    resp=$(curl -s -S -g --insecure "https://$FW_IP/api/?type=op&cmd=<request><content><upgrade><download><latest></latest></download></upgrade></content></request>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
@@ -24,7 +26,7 @@ while true
    fi
   sleep 10s
 done
-
+echo "Install content"
 while true
   do
    resp=$(curl -s -S -g --insecure "https://$FW_IP/api/?type=op&cmd=<request><content><upgrade><install><version>latest</version></install></upgrade></content></request>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
@@ -61,13 +63,13 @@ bash /usr/share/doc/wordpress/examples/setup-mysql -n Demo -t $DB_IP $DB_IP
 sed -i "s/define('DB_USER'.*/define('DB_USER', 'demouser');/g" /etc/wordpress/config-$DB_IP.php
 sed -i "s/define('DB_PASSWORD'.*/define('DB_PASSWORD', 'paloalto@123');/g" /etc/wordpress/config-$DB_IP.php
 mv /etc/wordpress/config-$DB_IP.php /etc/wordpress/config-$FW_PublicIP.php
-wget -O /usr/lib/cgi-bin/guess-sql-root-password.cgi https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-sample/guess-sql-root-password.cgi
+wget -O /usr/lib/cgi-bin/guess-sql-root-password.cgi https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-template/guess-sql-root-password.cgi
 chmod +x /usr/lib/cgi-bin/guess-sql-root-password.cgi
 sed -i "s/DB-IP-ADDRESS/$DB_IP/g" /usr/lib/cgi-bin/guess-sql-root-password.cgi
-wget -O /usr/lib/cgi-bin/ssh-to-db.cgi https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-sample/ssh-to-db.cgi
+wget -O /usr/lib/cgi-bin/ssh-to-db.cgi https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-template/ssh-to-db.cgi
 chmod +x /usr/lib/cgi-bin/ssh-to-db.cgi
 sed -i "s/DB-IP-ADDRESS/$DB_IP/g" /usr/lib/cgi-bin/ssh-to-db.cgi
-wget -O /var/www/html/sql-attack.html https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-sample/sql-attack.html
+wget -O /var/www/html/sql-attack.html https://raw.githubusercontent.com/PaloAltoNetworks/googlecloud/master/two-tier-template/sql-attack.html
 ln -sf /etc/apache2/conf-available/serve-cgi-bin.conf /etc/apache2/conf-enabled/serve-cgi-bin.conf
 ln -sf /etc/apache2/mods-available/cgi.load /etc/apache2/mods-enabled/cgi.load
 systemctl restart apache2
